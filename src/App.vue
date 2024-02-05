05486144f2b8ac43a55ec0ce3157e158b1a5c004
marcofinucci <info@marcofinucci.com>
2024-02-05 13:18:05 +0100

    generate 10 random emails

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data: () => ({
+    emails: [],
+  }),
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+        this.emails.push(response.data.response);
+      });
+    }
+  },
+};
+</script>
+
+<template>
+  <div v-for="email in emails" :key="email">
+    <p>{{ email }}</p>
+  </div>
+</template>
+
+<style scoped></style>
